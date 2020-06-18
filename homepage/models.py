a1adc99ab4c4c87798662f564648af35080877b3
Ben Luft <benluft8@gmail.com>
2020-06-17 21:35:12 -0600

    Basic Website working - cool styles and header link functionality working

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
+class BasicModel(models.Model):
+    basic_stuff = None
+
 # Create your models here.
--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from . import models
+
+page_nav_dict = {"Welcome": "landingpage:landing",
+                 "RSVP": "landingpage:rsvp",
+                 "Registry": "landingpage:landing",
+                 "Find our Location": "landingpage:landing"}
+
+
+class LandingPage(generic.TemplateView):
+
+    template_name = r'homepage/landingpage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['page_names'] = page_nav_dict
+        return context
+
+class RSVPPage(generic.TemplateView):
+
+    template_name = r'homepage/rsvp.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['page_names'] = page_nav_dict
+        return context
 
 # Create your views here.
